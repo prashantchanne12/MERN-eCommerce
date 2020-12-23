7c47620d0d58e4ce9ecd4f52d8efa029cc6bad3a
prashantchanne12 <prashantchanne121@gmail.com>
2020-12-23 12:14:57 +0530

    dispatching action from cart screen

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -13,9 +13,20 @@ const CartScreen = ({match, location, history}) => {
 
     const productId = match.params.id;
 
-    const qty = location.search;
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
 
-    console.log(qty);
+    const dispatch = useDispatch();
+
+    const cart = useSelector(state => state.cart);
+    const { cartItems } = cart;
+
+    useEffect(() => {
+
+        if(productId){
+            dispatch(addToCart(productId, qty))
+        }
+
+    },[dispatch, productId, qty])
 
     return (
         <div>
