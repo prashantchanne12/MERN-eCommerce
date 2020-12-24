2175ba2566b6f7e451a60ce9b66f629016f4e43b
prashantchanne12 <prashantchanne121@gmail.com>
2020-12-24 15:06:48 +0530

    created profile dropdown at navbar

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,9 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants";
+import {
+    USER_LOGIN_FAIL,
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGOUT
+} from "../constants/userConstants";
 import axios from 'axios';
 
 export const login = (email, password) => async (dispatch) => {
@@ -38,4 +43,12 @@ export const login = (email, password) => async (dispatch) => {
         })
 
     }
+}
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo');
+
+    dispatch({
+        type: USER_LOGOUT
+    });
 }
