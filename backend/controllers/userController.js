479deb45d66e39771bc1f516e4eb3d2495aaede6
prashantchanne12 <prashantchanne121@gmail.com>
2020-12-24 12:48:57 +0530

    created auth middleware to verify jwt token and route to view profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,6 +26,28 @@ const authUser = asyncHandler(async (req, res, next) => {
 
 });
 
+// @desc   Get user profile
+// @route  GET /api/users/profile
+// @access private
+const getUserProfile = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+
+        res.send({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+});
+
 export {
     authUser,
+    getUserProfile,
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 const userRouter = express.Router();
 
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 userRouter.post('/login', authUser);
+
+userRouter.get('/profile', protect, getUserProfile);
 
 export default userRouter;
