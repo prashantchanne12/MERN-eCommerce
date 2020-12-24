7266e58de5014b8393474858b3bef03b2d4c8417
prashantchanne12 <prashantchanne121@gmail.com>
2020-12-24 13:25:55 +0530

    redux setup for user login

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,18 +5,23 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 // Reducers
 import { productListReducer, productDetailsReducer } from './reducers/productReducer';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 // Combine reducers
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage }
+    cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage }
 };
 
 const middleware = [thunk];
