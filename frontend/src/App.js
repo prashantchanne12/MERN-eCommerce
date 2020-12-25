8ceb241ef90a1782b1f84c9eb3dcdd5cb0fc15da
prashantchanne12 <prashantchanne121@gmail.com>
2020-12-25 13:15:30 +0530

    created place order page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Container>
           <Route path='/shipping' component={ShippingScreen} />
           <Route path='/payment' component={PaymentScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' exact component={RegisterScreen} />
           <Route path='/profile' exact component={ProfileScreen} />
