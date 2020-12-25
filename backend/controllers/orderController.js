ce7d267ecb33ecdff3b486777d5de0ba734b3c62
prashantchanne12 <prashantchanne121@gmail.com>
2020-12-25 20:05:27 +0530

    added route for update the order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -57,7 +57,33 @@ const getOrderById = asyncHandler(async (req, res, next) => {
 
 });
 
+// @desc Update order to paid
+// @route GET /api/orders/:id/pay
+// @access Private
+const updateOrderToPaid = asyncHandler(async (req, res, next) => {
+
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+        const updatedOrder = await order.save();
+        res.send(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+
+});
+
 export {
     addOrderItems,
     getOrderById,
+    updateOrderToPaid,
 }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 const orderRouter = express.Router();
-import { addOrderItems, getOrderById } from '../controllers/orderController.js';
+import {
+    addOrderItems,
+    getOrderById,
+    updateOrderToPaid
+} from '../controllers/orderController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 orderRouter.post('/', protect, addOrderItems);
 orderRouter.get('/:id', protect, getOrderById);
+orderRouter.put('/:id/pay', protect, updateOrderToPaid);
 
 export default orderRouter;
