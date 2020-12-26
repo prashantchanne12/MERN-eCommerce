8e097ce325003428da437c492d8293ead38bfb5c
prashantchanne12 <prashantchanne121@gmail.com>
2020-12-26 19:21:23 +0530

    created route for get and update single user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,16 +8,22 @@ import {
     registerUser,
     updateUserProfile,
     deleteUser,
+    getUserById,
+    updateUser,
 } from '../controllers/userController.js';
 import { protect, admin } from '../middlewares/authMiddleware.js';
 
 userRouter.post('/', registerUser);
-userRouter.get('/', protect, admin, getUsers);
-userRouter.delete('/:id', protect, admin, deleteUser);
-
 userRouter.post('/login', authUser);
 
 userRouter.get('/profile', protect, getUserProfile);
 userRouter.put('/profile', protect, updateUserProfile);
+
+userRouter.get('/', protect, admin, getUsers);
+userRouter.delete('/:id', protect, admin, deleteUser);
+userRouter.get('/:id', protect, admin, getUserById);
+userRouter.put('/:id', protect, admin, updateUser);
+
+
 
 export default userRouter;
